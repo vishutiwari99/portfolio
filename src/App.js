4792b2a85f627e616caa0efec6b7e73daa7e2c7e
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-25 12:43:02 +0530

    imported pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import About from './components/About';
 import Header from './components/Header';
+import Intro from './components/Intro';
+import Skills from './components/Skills';
 
 
 function App() {
   return (
-    <Header />
-
-
+    <>
+      <Header />
+      <Intro />
+      <About />
+      <Skills />
+    </>
   );
 }
 
